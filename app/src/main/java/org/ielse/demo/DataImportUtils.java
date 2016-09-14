17e5cf7918dd14c334832d89b5194b9749250e86
else <18872260424@163.com>
2016-09-14 15:38:07 +0800

    change image url

--- app/src/main/java/org/ielse/demo/DataImportUtils.java
+++ app/src/main/java/org/ielse/demo/DataImportUtils.java
@@ -10,50 +10,50 @@ public class DataImportUtils {
 
     public static List<DataInfo> init() {
         DataInfo r1 = new DataInfo();
-        r1.cover = "http://img0.imgtn.bdimg.com/it/u=2489291458,3252859626&fm=11&gp=0.jpg";
-        r1.logo = "http://img2.duitang.com/uploads/item/201301/20/20130120194741_3if2E.thumb.600_0.png";
+        r1.cover = R.mipmap.a;
+        r1.logo = R.mipmap.ab;
         r1.title = "10 Free 4 X 6 Prints";
         r1.subTitle = "Online Code";
 
         DataInfo r2 = new DataInfo();
-        r2.cover = "http://tupian.enterdesk.com/2016/hxj/08/16/1602/ChMkJlexsJmIIe8dAAghrgQhdMQAAUdNAJInfYACCHG699.jpg";
-        r2.logo = "http://pic22.nipic.com/20120708/8972260_202111240352_2.jpg";
+        r2.cover = R.mipmap.b;
+        r2.logo = R.mipmap.aa;
         r2.title = "$20 Off $85+";
         r2.subTitle = "In-Store & Online";
 
         DataInfo r3 = new DataInfo();
-        r3.cover = "http://img2.imgtn.bdimg.com/it/u=1800131217,1063103501&fm=11&gp=0.jpg";
-        r3.logo = "http://img2.duitang.com/uploads/item/201301/20/20130120194741_3if2E.thumb.600_0.png";
+        r3.cover = R.mipmap.c;
+        r3.logo = R.mipmap.ab;
         r3.title = "$30 Off $90";
         r3.subTitle = "Online Code";
 
         DataInfo r4 = new DataInfo();
-        r4.cover = "http://img4.imgtn.bdimg.com/it/u=1680976998,1509734632&fm=11&gp=0.jpg";
-        r4.logo = "http://img2.duitang.com/uploads/item/201301/20/20130120194741_3if2E.thumb.600_0.png";
+        r4.cover = R.mipmap.d;
+        r4.logo = R.mipmap.ab;
         r4.title = "Free Lip Gloss on $15+";
         r4.subTitle = "Online Code";
 
         DataInfo r5 = new DataInfo();
-        r5.cover = "http://desk.fd.zol-img.com.cn/t_s960x600c5/g5/M00/00/0A/ChMkJlecZcyIH-BIAAQVSYqHlFsAAT_2gMTwTMABBVh401.jpg";
-        r5.logo = "http://pic22.nipic.com/20120708/8972260_202111240352_2.jpg";
+        r5.cover = R.mipmap.e;
+        r5.logo = R.mipmap.aa;
         r5.title = "30% Off Select Prices";
         r5.subTitle = "Online Sale";
 
         DataInfo r6 = new DataInfo();
-        r6.cover = "http://desk.fd.zol-img.com.cn/t_s960x600c5/g5/M00/02/06/ChMkJlbKyqeIWBwBADlexJVOKHMAALIewOsURQAOV7c671.jpg";
-        r6.logo = "http://img2.duitang.com/uploads/item/201301/20/20130120194741_3if2E.thumb.600_0.png";
+        r6.cover = R.mipmap.f;
+        r6.logo = R.mipmap.ab;
         r6.title = "Up to 70% Off";
         r6.subTitle = "Labor Day Online Sale";
 
         DataInfo r7 = new DataInfo();
-        r7.cover = "http://image99.360doc.com/DownloadImg/2016/08/2210/78467867_1.jpg";
-        r7.logo = "http://img2.duitang.com/uploads/item/201301/20/20130120194741_3if2E.thumb.600_0.png";
+        r7.cover = R.mipmap.g;
+        r7.logo = R.mipmap.ab;
         r7.title = "Play to Win Up to $50";
         r7.subTitle = "You could even win $5k!";
 
         DataInfo r8 = new DataInfo();
-        r8.cover = "http://img.51ztzj.com/upload/image/20160727/201607266_670x419.jpg";
-        r8.logo = "http://pic22.nipic.com/20120708/8972260_202111240352_2.jpg";
+        r8.cover = R.mipmap.h;
+        r8.logo = R.mipmap.aa;
         r8.title = "Fares from $39 One Way";
         r8.subTitle = "Hot 2 Day Sale";
 
--- app/src/main/java/org/ielse/demo/DataInfo.java
+++ app/src/main/java/org/ielse/demo/DataInfo.java
@@ -4,8 +4,8 @@ package org.ielse.demo;
  * Created by LY on 2016/9/9.
  */
 public class DataInfo {
-    public String logo;
-    public String cover;
+    public int logo;
+    public int cover;
     public String title;
     public String subTitle;
 }
